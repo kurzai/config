c01e83fd9c3ad3c6d407820b66a38831fecc5d21
Daniel Kurz <danielkurz@localhost.localdomain>
2021-09-15 18:34:19 +0200

    add root setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt full-upgrade instead
+
+sudo apt-get install geany-plugins
+sudo apt install gnome-builder
+
+# Install Bitwarden
+
+# Install npm, yarn
+
+./install_brave.sh
+
+sudo apt install chromium-browser
