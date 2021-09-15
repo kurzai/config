4552ad54177e447fcb0466842abfbc396a8c53b0
Daniel Kurz <danielkurz@localhost.localdomain>
2021-09-15 19:47:25 +0200

    install more software

---
+++ install_chrome_unstable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb
+sudo apt install ./google-chrome-unstable_current_amd64.deb
+rm ./google-chrome-unstable_current_amd64.deb
--- setup.sh
+++ setup.sh
@@ -12,3 +12,10 @@ sudo apt install gnome-builder
 sudo apt install chromium
 
 sudo apt install npm
+
+sudo apt -y install node-typescript
+sudo npm i -g ts-node node-gyp
+
+sudo apt -y install handbrake-cli
+
+./install_chrome_unstable.sh
