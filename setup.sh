9493c1b2aa7280bcfdd89e1eae08ad441a84e8d8
Daniel Kurz <danielkurz@localhost.localdomain>
2021-09-15 19:28:47 +0200

    install more software

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+# Install Bitwarden
 
 sudo apt update
 sudo apt full-upgrade instead
 
-sudo apt-get install geany-plugins
+sudo apt install geany-plugins
 sudo apt install gnome-builder
-
-# Install Bitwarden
-
-# Install npm, yarn
 
 ./install_brave.sh
 
-sudo apt install chromium-browser
+sudo apt install chromium
+
+sudo apt install npm
