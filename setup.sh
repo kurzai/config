392780abbb2134f2d149460f09f0211cca002167
Daniel Kurz <danielkurz@localhost.localdomain>
2021-09-17 13:30:12 +0200

    more software

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@
 sudo apt update
 sudo apt full-upgrade instead
 
-sudo apt install geany-plugins
-sudo apt install gnome-builder
+sudo apt -y install geany-plugins
+sudo apt -y install gnome-builder
 
 ./install_brave.sh
 
-sudo apt install chromium
+sudo apt -y install chromium
 
-sudo apt install npm
+sudo apt -y install npm
 
 sudo apt -y install node-typescript
 sudo npm i -g ts-node node-gyp
@@ -19,3 +19,5 @@ sudo npm i -g ts-node node-gyp
 sudo apt -y install handbrake-cli
 
 ./install_chrome_unstable.sh
+
+sudo apt -y install mediainfo
