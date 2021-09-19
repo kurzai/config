13f9c8948b30941d2fcee7692695165d841a0fd6
Daniel Kurz <danielkurz03@protonmail.com>
2021-09-19 16:44:21 +0200

    adjust for pop os

--- setup.sh
+++ setup.sh
@@ -4,12 +4,8 @@
 sudo apt update
 sudo apt full-upgrade instead
 
-sudo apt -y install geany-plugins
-sudo apt -y install gnome-builder
 
-./install_brave.sh
-
-sudo apt -y install chromium
+sudo apt -y install chromium-browser
 
 sudo apt -y install npm
 
@@ -17,7 +13,14 @@ sudo apt -y install node-typescript
 sudo npm i -g ts-node node-gyp
 
 sudo apt -y install handbrake-cli
+sudo apt -y install mediainfo
 
 ./install_chrome_unstable.sh
 
-sudo apt -y install mediainfo
+# apps to install
+# - vscodium
+# - bitwarden
+
+# git
+git config --global user.name "Daniel Kurz"
+git config --global user.email danielkurz03@protonmail.com
